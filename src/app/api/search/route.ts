fee4684a9ff69d9a8a5ed880160a307818e981bf
mariakaufmann <mari.kaufmann20@gmail.com>
2024-04-24 17:09:33 +0300

    migrated from planetscale to neon

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -1,18 +1,19 @@
-import { db } from "@/lib/db"
+import { db } from "@/lib/db";
 
 export async function GET(req: Request) {
-	const url = new URL(req.url)
-	
-	const q = url.searchParams.get('q')
-	console.log(q);
-	
-	if (!q) return new Response('Invalid query', {status: 400})
-	const results = await db.subsaddit.findMany({
-		where: {
-			name: {
-				startsWith: q
-			}
-		}
-	})
-	return new Response(JSON.stringify(results))
+  const url = new URL(req.url);
+
+  const q = url.searchParams.get("q");
+  console.log(q);
+
+  if (!q) return new Response("Invalid query", { status: 400 });
+  const results = await db.subsaddit.findMany({
+    where: {
+      name: {
+        startsWith: q,
+        mode: "insensitive",
+      },
+    },
+  });
+  return new Response(JSON.stringify(results));
 }
